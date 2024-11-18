2c2b57fc11eaed593044ef94c150ef9bb27dd8dc
zbkm <zubokome@gmail.com>
2024-11-18 19:11:17 +0500

    add signExtend, equality, and, or, xor, not, byte, shl, shr, sar operation

--- src/Utils/HexMath.php
+++ src/Utils/HexMath.php
@@ -7,36 +7,235 @@ use GMP;
 
 class HexMath
 {
+    /**
+     * Arithmetic (signed) right shift operation
+     *
+     * Shift the bits towards the least significant one. The bits moved before the first one are discarded, the new bits are set to 0 if the previous most significant bit was 0, otherwise the new bits are set to 1
+     *
+     * @param Hex $shift number of bits to shift to the right
+     * @param Hex $value 32 integer to shift
+     * @return Hex value >> shift: the shifted value
+     */
+    public static function sar(Hex $shift, Hex $value): Hex
+    {
+        if (gmp_cmp($shift->gmp(), 255) > 0) {
+            return Hex::from("0");
+        }
+
+        $result = $value->gmp() >> $shift->gmp();
+        if (0 > gmp_cmp(HexMath::toSigned($value)->gmp(), 0)) {
+            $mask = gmp_sub(self::modulo(), gmp_pow(2, 256 - $shift->getInt()));
+            $result = gmp_or($result, $mask);
+        }
+
+        return Hex::from(gmp_strval($result, 16));
+    }
+
+
+    /**
+     * Logical right shift operation
+     *
+     * Shift the bits towards the least significant one. The bits moved before the first one are discarded, the new bits are set to 0
+     *
+     * @param Hex $num   number of bits to shift to the right
+     * @param Hex $value 32 bytes to shift
+     * @return Hex value >> shift: the shifted value. If shift is bigger than 255, returns 0
+     */
+    public static function shr(Hex $num, Hex $value): Hex
+    {
+        if (gmp_cmp($num->gmp(), 255) > 0) {
+            return Hex::from("0");
+        }
+
+        $result = $value->gmp() >> $num->gmp();
+        $result = gmp_and($result, gmp_sub(self::modulo(), 1));
+
+        return Hex::from($result);
+    }
+
+    /**
+     * Left shift operation
+     *
+     * Shift the bits towards the most significant one. The bits moved after the 256th one are discarded, the new bits are set to 0
+     *
+     * @param Hex $shift number of bits to shift to the left
+     * @param Hex $value 32 bytes to shift
+     * @return Hex value << shift: the shifted value. If shift is bigger than 255, returns 0
+     */
+    public static function shl(Hex $shift, Hex $value): Hex
+    {
+        if (gmp_cmp($shift->gmp(), 255) > 0) {
+            return Hex::from("0");
+        }
+
+        $result = $value->gmp() << $shift->gmp();
+        $result = gmp_and($result, gmp_sub(self::modulo(), 1));
+
+        return Hex::from($result);
+    }
+
+    /**
+     * Retrieve single byte from word
+     *
+     * @param Hex $i     byte offset starting from the most significant byte
+     * @param Hex $value 32-byte value
+     * @return Hex the indicated byte at the least significant position. If the byte offset is out of range, the result is 0
+     */
+    public static function byte(Hex $i, Hex $value): Hex
+    {
+        $index = gmp_intval($i->gmp());
+
+        if ($index < 0 || $index >= 32) {
+            return Hex::from("0");
+        }
+
+        $shifted = gmp_div_q($value->gmp(), gmp_pow(2, (31 - $index) * 8));
+        $byte = gmp_and($shifted, 0xFF);
+
+        return Hex::from($byte);
+    }
+
+    /**
+     * Bitwise NOT operation
+     *
+     * @param Hex $value value
+     * @return Hex bitwise NOT result
+     */
+    public static function not(Hex $value): Hex
+    {
+        $result = ~$value->gmp();
+        $result = gmp_mod($result, self::modulo());
+
+        return Hex::from($result);
+    }
+
+    /**
+     * Bitwise XOR operation
+     *
+     * @param Hex $a first value
+     * @param Hex $b second value
+     * @return Hex bitwise XOR result
+     */
+    public static function xor(Hex $a, Hex $b): Hex
+    {
+        return Hex::from(gmp_xor($a, $b));
+    }
+
+    /**
+     * Bitwise OR operation
+     *
+     * @param Hex $a first value
+     * @param Hex $b second value
+     * @return Hex bitwise OR result
+     */
+    public static function or(Hex $a, Hex $b): Hex
+    {
+        return Hex::from(gmp_or($a, $b));
+    }
+
+    /**
+     * Bitwise AND operation
+     *
+     * @param Hex $a first value
+     * @param Hex $b second value
+     * @return Hex bitwise AND result
+     */
+    public static function and(Hex $a, Hex $b): Hex
+    {
+        return Hex::from(gmp_and($a, $b));
+    }
+
+    /**
+     * Equality comparison
+     *
+     * @param Hex $a first
+     * @param Hex $b second
+     * @return bool a == 0: 1 if a is 0, 0 otherwise
+     */
+    public static function eq(Hex $a, Hex $b): bool
+    {
+        return $a->get() === $b->get();
+    }
+
+    /**
+     * Format to signed value
+     *
+     * @param Hex $value value
+     * @return Hex
+     */
+    public static function toSigned(Hex $value): Hex
+    {
+        $two_complement = gmp_pow(2, 255);
+        $result =  gmp_sub(gmp_mod(gmp_add($value->gmp(), $two_complement), self::modulo()), $two_complement);
+
+        return Hex::from($result);
+    }
+
+    /**
+     * Exponential operation
+     *
+     * @param Hex $a base
+     * @param Hex $b exponent
+     * @return Hex
+     */
     public static function exp(Hex $a, Hex $b): Hex
     {
         $result = gmp_pow($a->gmp(), gmp_intval($b->gmp()));
         $result = gmp_mod($result, self::modulo());
-        return Hex::from(gmp_strval($result, 16));
+        return Hex::from($result);
     }
 
+    /**
+     * Addition operation
+     *
+     * @param Hex $a first value to add
+     * @param Hex $b second value to add
+     * @return Hex
+     */
     public static function sum(Hex $a, Hex $b): Hex
     {
         $result = gmp_add($a->gmp(), $b->gmp());
         $result = gmp_mod($result, self::modulo());
-        return Hex::from(gmp_strval($result, 16));
+        return Hex::from($result);
     }
 
+    /**
+     * Multiplication operation
+     *
+     * @param Hex $a first value to multiply
+     * @param Hex $b second value to multiply
+     * @return Hex
+     */
     public static function mul(Hex $a, Hex $b): Hex
     {
         $result = gmp_mul($a->gmp(), $b->gmp());
         $result = gmp_mod($result, self::modulo());
 
-        return Hex::from(gmp_strval($result, 16));
+        return Hex::from($result);
     }
 
+    /**
+     * Subtraction operation
+     *
+     * @param Hex $a first value
+     * @param Hex $b value to subtract to the first
+     * @return Hex
+     */
     public static function sub(Hex $a, Hex $b): Hex
     {
         $result = gmp_sub($a->gmp(), $b->gmp());
         $result = gmp_mod($result, self::modulo());
 
-        return Hex::from(gmp_strval($result, 16));
+        return Hex::from($result);
     }
 
+    /**
+     * Integer division operation
+     *
+     * @param Hex $a numerator
+     * @param Hex $b denominator
+     * @return Hex
+     */
     public static function div(Hex $a, Hex $b): Hex
     {
         if ($b->get() === "0") {
@@ -44,9 +243,16 @@ class HexMath
         }
 
         $sum = gmp_div($a->get(), $b->get());
-        return Hex::from(gmp_strval($sum, 16));
+        return Hex::from($sum);
     }
 
+    /**
+     * Signed integer division operation (truncated)
+     *
+     * @param Hex $a numerator
+     * @param Hex $b denominator
+     * @return Hex
+     */
     public static function sdiv(Hex $a, Hex $b): Hex
     {
         $two_complement = gmp_pow(2, 255);
@@ -56,9 +262,16 @@ class HexMath
         $result = gmp_cmp($signedB, 0) == 0 ? 0 : gmp_div_q($signedA, $signedB);
         $result = gmp_mod(gmp_add($result, self::modulo()), self::modulo());
 
-        return Hex::from(gmp_strval($result, 16));
+        return Hex::from($result);
     }
 
+    /**
+     * Modulo remainder operation
+     *
+     * @param Hex $a numerator
+     * @param Hex $b denominator
+     * @return Hex
+     */
     public static function mod(Hex $a, Hex $b): Hex
     {
         if ($b->get() === "0") {
@@ -66,9 +279,16 @@ class HexMath
         }
 
         $mod = gmp_mod($a->gmp(), $b->gmp());
-        return Hex::from(gmp_strval($mod, 16));
+        return Hex::from($mod);
     }
 
+    /**
+     * Signed modulo remainder operation
+     *
+     * @param Hex $a numerator
+     * @param Hex $b denominator
+     * @return Hex
+     */
     public static function smod(Hex $a, Hex $b): Hex
     {
         $two_complement = gmp_pow(2, 255);
@@ -86,10 +306,52 @@ class HexMath
         }
 
         $result = gmp_mod(gmp_add($result, self::modulo()), self::modulo());
-        return Hex::from(gmp_strval($result, 16));
+        return Hex::from($result);
 
     }
 
+    /**
+     * Extend length of two’s complement signed integer
+     *
+     * @param Hex $b size in byte
+     * @param Hex $x value to sign extend
+     * @return Hex
+     */
+    public static function signExtend(Hex $b, Hex $x): Hex
+    {
+        $bitIndex = gmp_add(gmp_mul($b->gmp(), 8), 7);
+        if ($bitIndex >= 256) {
+            return $x; // if size > 256 bit, return value not edited
+        }
+
+        $mask = gmp_sub(gmp_pow(2, gmp_intval($bitIndex + 1)), 1); // mask
+        $result = gmp_and($x->gmp(), $mask); // only high-order bits
+
+        if (gmp_testbit($x->gmp(), gmp_intval($bitIndex))) { // if MSb = 1
+            $result = gmp_or($result, ~gmp_and($mask, gmp_sub(self::modulo(), 1)));
+        }
+        $result = gmp_mod(gmp_add($result, self::modulo()), self::modulo());
+
+        return Hex::from($result);
+    }
+
+    /**
+     * Compare two hex
+     *
+     * @param Hex $a left side
+     * @param Hex $b right side
+     * @return int a positive value if a > b, zero if a = b and a negative value if a < b
+     */
+    public static function cmp(Hex $a, Hex $b): int
+    {
+        return gmp_cmp($a->gmp(), $b->gmp());
+    }
+
+    /**
+     * 2 ^ 256
+     *
+     * @return GMP
+     */
     protected static function modulo(): GMP
     {
         return gmp_pow(2, 256);
--- tests/Utils/HexMathTest.php
+++ tests/Utils/HexMathTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Utils;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use Zbkm\Evm\Utils\Hex;
 use Zbkm\Evm\Utils\HexMath;
 use PHPUnit\Framework\TestCase;
@@ -45,7 +46,7 @@ class HexMathTest extends TestCase
         $a = Hex::from("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
         $b = Hex::from("2");
         $result = HexMath::mul($a, $b);
-        $this->assertEquals(gmp_strval("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE",16), $result->get());
+        $this->assertEquals(gmp_strval("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE", 16), $result->get());
     }
 
     public function testSub(): void
@@ -116,5 +117,119 @@ class HexMathTest extends TestCase
         $b = Hex::from("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD");
         $result = HexMath::smod($a, $b);
         $this->assertEquals(gmp_strval("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE", 16), $result->get());
+    }
+
+
+    public static function signExtendProvider(): array
+    {
+        return [
+            ["80", "0x126af4", "0x126af4"], // Invalid Byte Number
+            ["0", "0", "0"], // 0 0
+            ["0", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"], // 0 -1
+            ["0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe", "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe", "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe"], // -2 -2
+            ["0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"], // -1 -1
+            ["0xf00000000000000001", "0xff", "0xff"], // <lagre number> 255
+            ["0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", "0x00", "0x00"], // -1 0
+            ["0", "0x122f6a", "0x6a"], // bit is not set
+            ["1", "0x126af4", "0x6af4"], // bit is not set in higher byte
+            ["1", "0x12faf4", "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffaf4"], // bit is set in higher byte
+            ["0x010000000000000001", "0x8000", "0x8000"], // overflow the byte number value
+            ["0xf0000000000001", "0xFFFF", "0xFFFF"], // try to overflow the byte number value
+            ["0", "0x122ff4", "0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff4"], // set bit (0, 0x<whatever>ff)
+            ["31", "1", "0x01"], // 31, positive value
+            ["31", HexMath::sub(Hex::from(0), Hex::from(1))->get(), "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"], // 31, negative value
+        ];
+    }
+
+    #[DataProvider("signExtendProvider")]
+    public function testSignExtend(string $aValue, string $bValue, string $expected): void
+    {
+        $a = Hex::from($aValue);
+        $b = Hex::from($bValue);
+        $result = HexMath::signExtend($a, $b);
+        $this->assertEquals(Hex::from($expected)->get(), $result->get());
+    }
+
+    public static function byteProvider(): array
+    {
+        return [
+            ["1F", "0xFF", "0xFF"],
+            ["1E", "0xFF00", "0xFF"],
+        ];
+    }
+
+    #[DataProvider("byteProvider")]
+    public function testByte(string $iValue, string $xValue, string $expected): void
+    {
+        $i = Hex::from($iValue);
+        $x = Hex::from($xValue);
+        $result = HexMath::byte($i, $x);
+        $this->assertEquals(Hex::from($expected)->get(), $result->get());
+    }
+
+    public static function shlProvider(): array
+    {
+        return [
+            ["1", "1", "2"],
+            ["4", "0xFF00000000000000000000000000000000000000000000000000000000000000", "0xF000000000000000000000000000000000000000000000000000000000000000"],
+        ];
+    }
+
+    #[DataProvider("shlProvider")]
+    public function testShl(string $nValue, string $xValue, string $expected): void
+    {
+        $n = Hex::from($nValue);
+        $x = Hex::from($xValue);
+        $result = HexMath::shl($n, $x);
+        $this->assertEquals(Hex::from($expected)->get(), $result->get());
+    }
+
+    public static function shrProvider(): array
+    {
+        return [
+            ["1", "2", "1"],
+            ["4", "0xFF", "0xF"],
+        ];
+    }
+
+    #[DataProvider("shrProvider")]
+    public function testShr(string $nValue, string $xValue, string $expected): void
+    {
+        $n = Hex::from($nValue);
+        $x = Hex::from($xValue);
+        $result = HexMath::shr($n, $x);
+        $this->assertEquals(Hex::from($expected)->get(), $result->get());
+    }
+
+    public static function notProvider(): array
+    {
+        return [
+            ["0", "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"]
+        ];
+    }
+
+    #[DataProvider("notProvider")]
+    public function testNot(string $value, string $expected): void
+    {
+        $n = Hex::from($value);
+        $result = HexMath::not($n);
+        $this->assertEquals(Hex::from($expected)->get(), $result->get());
+    }
+
+    public static function sarProvider(): array
+    {
+        return [
+            ["1", "2", "1"],
+            ["4", "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF0", "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"],
+        ];
+    }
+
+    #[DataProvider("sarProvider")]
+    public function testSar(string $nValue, string $xValue, string $expected): void
+    {
+        $n = Hex::from($nValue);
+        $x = Hex::from($xValue);
+        $result = HexMath::sar($n, $x);
+        $this->assertEquals(Hex::from($expected)->get(), $result->get());
     }
 }
