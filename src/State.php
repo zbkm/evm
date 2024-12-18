2ee559147802dd80b9ff5a68192042449d0d0d84
zbkm <zubokome@gmail.com>
2024-12-18 14:45:22 +0500

    adding the necessary state data to the State class

--- src/State.php
+++ src/State.php
@@ -8,15 +8,45 @@ use AllowDynamicProperties;
 #[AllowDynamicProperties]
 class State
 {
+    /**
+     * @param string $from
+     * @param string $to
+     * @param string $value
+     * @param string $gasPrice
+     * @param int    $gasLimit
+     * @param string $calldata
+     * @param int    $block      current block number
+     * @param int    $timestamp  unix timestamp of the current block
+     * @param string $coinbase   miner's 20-byte address
+     * @param string $prevRandao previous block's RANDAO mix https://eips.ethereum.org/EIPS/eip-4399
+     * @param int    $chainId    chain id of the network
+     * @param int $baseFee    base fee in wei
+     * @param string $origin
+     * @param string $caller
+     */
     public function __construct(
         public string $from,
         public string $to,
-        public string $origin,
-        public string $caller,
         public string $value,
         public string $gasPrice,
+        public int $gasLimit,
         public string $calldata,
+        public int $block,
+        public int $timestamp,
+        public string $coinbase,
+        public string $prevRandao,
+        public int $chainId,
+        public int $baseFee,
+        public string $origin = "",
+        public string $caller = ""
     )
     {
+        if ($this->origin === "") {
+            $this->origin = $this->from;
+        }
+
+        if ($this->caller === "") {
+            $this->caller = $this->from;
+        }
     }
 }
